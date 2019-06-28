5d154d78e75cc3ad0a32ae76069ccd047145e99a
Victor Lopez <lvictor_9@hotmail.com>
2019-06-28 16:56:19 -0600

    Heroku server

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ const morgan = require('morgan') // logs requests
 const db = require('knex')({
    client: 'pg',
    connection: {
-     connectionString: process.env.DATABASE_URL,
+     connectionString: process.env.DATABASE_URL || 
+     "postgres://gjqlhvnhleneub:dd5149eb82a5b15ad13719f5a237e402d50e9d23e7c8ac72daf918f9381d22b6@ec2-54-204-35-248.compute-1.amazonaws.com:5432/d7mtiics2ervga",
      ssl: true,
    }
 });
